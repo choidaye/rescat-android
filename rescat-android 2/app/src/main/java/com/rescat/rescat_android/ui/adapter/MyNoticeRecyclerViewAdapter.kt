2d5c6c7e2c23009c2806252bf131cdcfe96ea2ab
choidaye <soar_dy@naver.com>
2019-01-09 04:35:33 +0900

    케어테이커 시 내주소 보이는거 뷰

---
+++ rescat-android 2/app/src/main/java/com/rescat/rescat_android/ui/adapter/MyNoticeRecyclerViewAdapter.kt
@@ -0,0 +1,44 @@
+package com.rescat.rescat_android.ui.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.rescat.rescat_android.Get.GetMyNoticeResponse
+import com.rescat.rescat_android.R
+import com.rescat.rescat_android.ui.activity.helpcat.AdoptActivity
+import org.jetbrains.anko.startActivity
+
+
+class MyNoticeRecyclerViewAdapter(val ctx: Context, val noticelist: ArrayList<GetMyNoticeResponse>) : RecyclerView.Adapter<MyNoticeRecyclerViewAdapter.Holder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  MyNoticeRecyclerViewAdapter.Holder {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.rv_item_notice, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = noticelist.size
+
+    override fun onBindViewHolder(holder:  MyNoticeRecyclerViewAdapter.Holder, position: Int) {
+
+        holder.contents.text = noticelist[position].notification.contents
+        holder.creatAt.text = noticelist[position].createdAt
+
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val creatAt: TextView = itemView.findViewById(R.id.tv_rv_item_notice_date) as TextView
+        val contents : TextView = itemView.findViewById(R.id.tv_rv_item_notice_content) as TextView
+
+    }
+
+
+}
--- rescat-android 2/app/src/main/java/com/rescat/rescat_android/ui/adapter/MyPageLocationRecyclerViewAdapter.kt
+++ rescat-android 2/app/src/main/java/com/rescat/rescat_android/ui/adapter/MyPageLocationRecyclerViewAdapter.kt
@@ -11,33 +11,37 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.rescat.rescat_android.Get.GetMyNoticeResponse
 import com.rescat.rescat_android.R
 import com.rescat.rescat_android.model.RegionData
 import com.rescat.rescat_android.ui.activity.helpcat.AdoptActivity
 import org.jetbrains.anko.startActivity
 
 
-class MyPageLocationAdapter(val ctx: Context, val regionList: ArrayList<RegionData>) : RecyclerView.Adapter<MyPageLocationAdapter.Holder>(){
+class MyPageLocationRecyclerViewAdapter(val ctx: Context, val locationlist: ArrayList<RegionData>) : RecyclerView.Adapter<MyPageLocationRecyclerViewAdapter.Holder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageLocationAdapter.Holder {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageLocationRecyclerViewAdapter.Holder {
         val view = LayoutInflater.from(ctx).inflate(R.layout.rv_item_my_address, parent, false)
-
-
         return Holder(view)
     }
 
-    override fun getItemCount(): Int = regionList.size
+    override fun getItemCount(): Int = locationlist.size
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.name.text = regionList[position].name
+    override fun onBindViewHolder(holder: MyPageLocationRecyclerViewAdapter.Holder, position: Int) {
+
+        holder.name.text = locationlist[position].name
+
+        Log.e("viewholder","뷰홀더 완료")
 
 
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name : TextView = itemView.findViewById(R.id.tv_fg_my_location_address) as TextView
+        val name: TextView = itemView.findViewById(R.id.tv_fg_my_location_address) as TextView
 
     }
+
 
 }
